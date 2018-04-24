a12539f9cb8cab7c733f248b0da9fe5fc68ca818
dmitryst666 <dmitryst666@gmail.com>
2018-04-24 14:41:22 +0400

    Context menus removed, sample *cat names* array added

--- app/src/main/java/localhost/shadow/sql_tst02/AddClients.java
+++ app/src/main/java/localhost/shadow/sql_tst02/AddClients.java
@@ -7,7 +7,9 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,35 +26,28 @@ public class AddClients extends Activity{
         setContentView(R.layout.activity_addclient);
 
         test1 = (TextView)findViewById(R.id.textView);
-        test1.setText("fndljg hgo oug horg erogoi og oiurogu orug ourogiurei gureuog rhghlgrugh984h iigreuhg 48 8934 t983 t0480 098t 40t04 t0948t094 308");
+        test1.setText("ტექსტის თვისებების შეცვლა");
         registerForContextMenu(test1);
         chb = (CheckBox)findViewById(R.id.chbExtMenu);
-    }
 
 
-    final int MENU_COLOR_RED = 1;
-    final int MENU_COLOR_GREEN = 2;
-    final int MENU_COLOR_BLUE = 3;
+        // получаем экземпляр элемента ListView
+        ListView listView = (ListView)findViewById(R.id.listView);
 
-    final int MENU_SIZE_12 = 4;
-    final int MENU_SIZE_14 = 5;
-    final int MENU_SIZE_18 = 6;
+        // определяем массив типа String
+        final String[] catNames = new String[] {
+                "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+                "Китти", "Масяня", "Симба"
+        };
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenu.ContextMenuInfo menuInfo) {
-        // TODO Auto-generated method stub
-                 menu.add(0, MENU_COLOR_RED, 0, "Red");
-                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
-                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, catNames);
 
-                menu.add(0, MENU_SIZE_12, 0, "12");
-                menu.add(0, MENU_SIZE_14, 0, "14");
-                menu.add(0, MENU_SIZE_18, 0, "18");
+        listView.setAdapter(adapter);
 
 
     }
-
 
     // создание меню
     @Override
@@ -95,34 +90,6 @@ public class AddClients extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        switch (item.getItemId()) {
-            // пункты меню для tvColor
-            case MENU_COLOR_RED:
-                test1.setTextColor(Color.RED);
-                break;
-            case MENU_COLOR_GREEN:
-                test1.setTextColor(Color.GREEN);
-                break;
-            case MENU_COLOR_BLUE:
-                test1.setTextColor(Color.BLUE);
-                break;
-// пункты меню для tvSize
-            case MENU_SIZE_12:
-                test1.setTextSize(12);
-                break;
-            case MENU_SIZE_14:
-                test1.setTextSize(14);
-                break;
-            case MENU_SIZE_18:
-                test1.setTextSize(18);
-                break;
-        }
-        return super.onContextItemSelected(item);
-    }
 
 
 }
